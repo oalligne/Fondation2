f37a489a97aabf0a368663a6f5e259660125907a
oalligne <olivier.alligne@gmail.com>
2018-08-19 16:58:09 +0200

    @OAL : Design test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,72 @@
 @extends('template')
 
 @section('contenu')
-    <br>
-	<div class="col-sm-offset-3 col-sm-6">
-		<div class="panel panel-info">
-			<div class="panel-heading">Contactez-moi</div>
-			<div class="panel-body"> 
-				{!! Form::open(['url' => 'contact']) !!}
-					<div class="form-group {!! $errors->has('nom') ? 'has-error' : '' !!}">
-						{!! Form::text('nom', null, ['class' => 'form-control', 'placeholder' => 'Votre nom']) !!}
-						{!! $errors->first('nom', '<small class="help-block">:message</small>') !!}
-					</div>
-					<div class="form-group {!! $errors->has('email') ? 'has-error' : '' !!}">
-						{!! Form::email('email', null, ['class' => 'form-control', 'placeholder' => 'Votre email']) !!}
-						{!! $errors->first('email', '<small class="help-block">:message</small>') !!}
-					</div>
-					<div class="form-group {!! $errors->has('texte') ? 'has-error' : '' !!}">
-						{!! Form::textarea ('texte', null, ['class' => 'form-control', 'placeholder' => 'Votre message']) !!}
-						{!! $errors->first('texte', '<small class="help-block">:message</small>') !!}
-					</div>
-					{!! Form::submit('Envoyer !', ['class' => 'btn btn-info pull-right']) !!}
-				{!! Form::close() !!}
-			</div>
+<br/>
+<div class="row" >
+	 <div class="col-md-3  offset-md-1">
+	    <div class="card border-primary">
+	      <div class="card-header">Quiz du jour</div>
+	      <div class="card-body">
+	        <p class="card-text">Essayez le quiz du jour pour remporter jusqu'à 50 points</p>
+	        <button type="button" class="btn btn-primary btn-sm">Démarrer >></button>
+	      </div>
+	    </div>
+    </div>
+    <div class="col-md-3">
+	    <div class="card">
+	      <div class="card-body">
+	        <h5 class="card-title">Classement</h5>
+	        <p class="card-text">1- Xirius 20 pts <br/>2- Ninou 15 pts <br/>3- John 8 pts<br/></p>
+	        <button type="button" class="btn btn-primary btn-sm">Voir tout le classement</button>
+	      </div>
+	    </div>
+    </div>
+    <div class="col-md-3">
+		<div class="card border-info">
+		  <div class="card-header">Nouveautés</div>
+		  <div class="card-body text-info">
+		    <h5 class="card-title">Nouvelles fonctionnalités</h5>
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		  </div>
 		</div>
 	</div>
+
+</div>
+<br/>
+<div class="row" >
+	<div class="col-md-10 offset-md-1" >
+	<h3>Quiz Aléatoires</h3>
+	<hr/>
+	</div>
+</div>
+<div class="row" >
+	<div class="col-md-3 offset-md-1">
+	    <div class="card">
+	      <div class="card-body">
+	        <h5 class="card-title">Les meilleurs morceaux de Beethoven</h5>
+	        <p class="card-text">- Sonate au clair de lune <br/> - Concerto pour violon <br/>- ... <br/></p>
+	        <button type="button" class="btn btn-primary btn-sm">Démarrer >></button>
+	      </div>
+	    </div>
+    </div>
+    <div class="col-md-3">
+	    <div class="card">
+	      <div class="card-body">
+	        <h5 class="card-title">Les opéras de Mozart</h5>
+	        <p class="card-text">- Sonate au clair de lune <br/> - Concerto pour violon <br/>- ... <br/></p>
+	        <button type="button" class="btn btn-primary btn-sm">Démarrer >></button>
+	      </div>
+	    </div>
+    </div>
+    <div class="col-md-3">
+	    <div class="card">
+	      <div class="card-body">
+	        <h5 class="card-title">Les concertos pour violon</h5>
+	        <p class="card-text">- Sonate au clair de lune <br/> - Concerto pour violon <br/>- ... <br/></p>
+	        <button type="button" class="btn btn-primary btn-sm">Démarrer >></button>
+	      </div>
+	    </div>
+    </div>
+</div>
+</div>
 @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -7,7 +7,8 @@
 		<title>Classiquiz</title>
 
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
+		
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 
 		<style> textarea { resize: none; } </style>
 	</head>
@@ -20,17 +21,29 @@
 
 		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 		    <ul class="navbar-nav mr-auto">
-		      <li class="nav-item active">
+
+		      <li class="nav-item dropdown">
+		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		          Quiz
+		        </a>
+		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          <a class="dropdown-item" href="#">Quiz du jour</a>
+		          <a class="dropdown-item" href="#">Quiz sur les compositeurs</a>
+		          <a class="dropdown-item" href="#">Quiz sur les styles</a>
+		        </div>
+		      </li>
+		       <li class="nav-item">
+		        <a class="nav-link" href="#">Compositeurs <span class="sr-only"></span></a>
+		      </li>
+		     </ul>
+		    <ul class="navbar-nav ml-auto">
+		      <li class="nav-item">
 		        <a class="nav-link" href="#">Register <span class="sr-only"></span></a>
 		      </li>
-		      <li class="nav-item active">
+		      <li class="nav-item">
 		        <a class="nav-link" href="#">Login <span class="sr-only"></span></a>
 		      </li>
-		    </ul>
-		    <form class="form-inline my-2 my-lg-0">
-		      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-		      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-		    </form>
+		      </ul>
 		  </div>
 		</nav>
 
